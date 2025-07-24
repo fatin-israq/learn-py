9356e03d647d95d1fa0d68a64c5ea0d13b32d6b1
Fatin Israq Talha <israqq2120@gmail.com>
2025-07-24 13:52:07 +0600

    Add initial implementation of how to import modules

---
+++ Importing-Packages/test.py
@@ -0,0 +1,6 @@
+from package.maths_by_israq import *
+
+print(addition(2, 3))           # outputs 5
+print(subtraction(3, 2))        # outputs 1
+print(multiplication(5, 3))     # outputs 15
+print(division(15, 3))          # outputs 5.0
